c34bb9b6c17f1912b29220e4dff35dfd06a584e3
Daniil <110263140+itshliakhov@users.noreply.github.com>
2022-10-05 20:24:18 +0300

    Update secondTask.js

--- secondTask.js
+++ secondTask.js
@@ -3,7 +3,7 @@
 */
 function newImage() {
   let _image = document.querySelector(".js--image");
-  _image.src = `/images/` + Math.ceil(Math.random() * 9) + `.png`;
+  _image.src = `./images/` + Math.ceil(Math.random() * 9) + `.png`;
   _image.style.width = `${window.outerWidth}` * 0.8 + `px`;
 }
 newImage();
